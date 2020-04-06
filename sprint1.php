c0191e35776b6a53bb0eeef17bf0a3304ab4218e
Helmutux <donatas.kulvinskas@gmail.com>
2020-04-06 21:58:52 +0300

    suformuota pradine struktura sprint1 uzduotyje

--- sprint1.php
+++ sprint1.php
@@ -9,7 +9,12 @@
         font-family: courier;
 
     }
-
+    h4{
+        display: inline-block;
+        width: 100%;
+        background: silver;
+        padding: 1em;
+    }
     a {
         display: inline-block;
         margin: 0.5em 0 0.5em 1em;
@@ -25,7 +30,27 @@
         margin-left: 25%;
         margin-right: 25%;
     }
+    .katalog{
+        display: block;
+        border: 1px solid #B3B898;
+        width: 280px;
+        background: #FEF6AB;
+        color: #000619;
+        padding: 0.2em;
+        margin: 0;
+        margin-block-start: 0.2em;
+        margin-block-end: 0;
+    }
 
+    .files{
+        display: block;
+        border: 1px solid #B3B898;
+        width: 280px;
+        color: #000619;
+        padding: 0.2em;
+        margin: 0;
+        margin-block-start: 0.2em;
+        margin-block-end: 0;}
     .task {
         font-size: 20px;
     }
@@ -51,10 +76,32 @@
         <h2>
         Sukurkite PHP failų naršyklę, kuri suteikia galimybę matyti failus ir/ar direktorijas.
         </h2>
-        <hr>
         <div class="task">
+        <h4>Pateikta direktorija 'C:\Program Files\Ampps\www\PHP-kurso-uzduotys'</h4>
+        <hr>
         <?php
-                     
+            //panaudojame $_SERVER['REQUEST_URI'];
+           echo "Dabar esame čia:<br>"; 
+           echo $_SERVER['REQUEST_URI'];
+           echo "<hr>";
+           //nurodome vieta(kataloga/direktorija), su kurios turiniu dirbsime 
+           $dir    = 'C:\Program Files\Ampps\www\PHP-kurso-uzduotys';
+           $turinys = scandir($dir);//panaudojame scandir, suformuojame string'u masyva (kiekvienas failas ar katalogas patampa atskiru masyvo elementu) 
+            
+            echo "<br>";
+            //atspausdiname suformuoto masyvo elementus ir gauname pasirinktos vietos turini
+            foreach($turinys as $k => $v){
+            //sukuriame salyga tikrindami ar elementas ne direktorija    
+            if(is_dir($v)==true){
+                print_r('<p class="katalog">'.$v.'</p>');//teigiamu atveju spausdindami paryskiname
+            }
+            //sukuriame salyga tikrindami ar elementas ne failas
+            elseif(is_file($v)==true){
+                print_r('<p class="files">'.$v.'</p>');//teigiamu atveju spausdindami suteikiame italic stiliu
+            }
+            $i++;//padidinam indeksa ir kartojam cikla tikrindami sekanti elementa
+            }
+           
         ?>
         <a href="#top"><b>Į puslapio viršų</b></a>
         </div>
