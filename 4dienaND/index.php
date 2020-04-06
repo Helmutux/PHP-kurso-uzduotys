e9a7abb2e462ef26401d8268c0c120ec64d55634
Helmutux <donatas.kulvinskas@gmail.com>
2020-04-06 13:50:12 +0300

    Sukurta ir prideta 4 dienos namu darbu katalogas

---
+++ 4dienaND/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4 dienos užduotys</title>
+</head>
+<body>
+    <a href="task1.php"><h2>1.Masyvo elementų atskyrimas kableliais</h2></a>
+    <a href="task2.php"><h2>2.Kas antro masyvo elemento išvedimas</h2></a>
+    <a href="task3.php"><h2>3.Daugybos lentelė</h2></a>
+    <a href="task3.php"><h2>4.PHP failų naršyklė</h2></a>
+</body>
+</html>
